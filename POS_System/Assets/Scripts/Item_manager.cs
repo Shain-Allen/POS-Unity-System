c40902cbf75333ee7ae981b68feba2202ecbd8fd
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2020-07-17 00:40:57 -0700

    Items can be added to the scrollable list now
    so much progress

---
+++ POS_System/Assets/Scripts/Item_manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_manager : MonoBehaviour
+{
+    public GameObject itemContainer;
+    public GameObject itemBlank;
+    public List<ItemBlank> inventory;
+
+    public void ItemEntry()
+    {
+        if(inventory.Count == 0)
+        {
+            Instantiate<GameObject>(itemBlank).transform.SetParent(itemContainer.transform, false);
+        }
+    }
+}
