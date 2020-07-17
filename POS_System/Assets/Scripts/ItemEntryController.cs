190b3078eb6f73182d50a834636b5ce31ce296a2
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2020-07-17 11:11:28 -0700

    slight roadblock with num pad

---
+++ POS_System/Assets/Scripts/ItemEntryController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemEntryController : MonoBehaviour
+{
+    public string itemCode;
+    public GameObject inputField;
+
+    public void StoreItemCode()
+    {
+        itemCode = inputField.GetComponent<TextMeshProUGUI>().text;
+        Debug.Log(itemCode);
+    }
+
+    public void NumberPadNumber(int number)
+    {
+        inputField.GetComponent<TextMeshProUGUI>().text += number.ToString();
+    }
+}
