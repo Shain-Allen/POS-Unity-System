8a4d3c54232bae3cbd1282745d43dbfa5b959c99
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2020-08-16 19:16:53 -0700

    fixed the item adding and started on quatiy button

--- POS_System/Assets/Scripts/Item_manager.cs
+++ POS_System/Assets/Scripts/Item_manager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item_manager : MonoBehaviour
 {
@@ -17,8 +18,7 @@ public class Item_manager : MonoBehaviour
     {
         if(inventory.Count == 0 || ItemEntryField.text == "")
         {
-            GameObject newItem = Instantiate(itemBlank);
-            newItem.transform.SetParent(itemContainer.transform);
+            GameObject newItem = Instantiate(itemBlank, itemContainer.transform, false);
         }
         else
         {
@@ -26,9 +26,22 @@ public class Item_manager : MonoBehaviour
 
             if (ItemLookUpCode.Length == 8)
             {
-                FindBySKU(Convert.ToUInt64(ItemLookUpCode));
-                GameObject newItem = Instantiate(itemBlank);
-                newItem.transform.SetParent(itemContainer.transform);
+                GameObject newItem;
+                ItemBlank itemInfo = FindBySKU(Convert.ToUInt64(ItemLookUpCode));
+                if (itemInfo == null)
+                {
+                    newItem = Instantiate(itemBlank, itemContainer.transform, false);
+
+                }
+                else
+                {
+                    newItem = Instantiate(itemBlank, itemContainer.transform, false);
+                    //Debug.Log(itemInfo.name);
+                    //Debug.Log(newItem.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text);
+                    newItem.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = itemInfo.name;
+                    newItem.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "$" + itemInfo.price.ToString();
+                    newItem.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = itemInfo.upc.ToString();
+                }
             }
         }
     }
